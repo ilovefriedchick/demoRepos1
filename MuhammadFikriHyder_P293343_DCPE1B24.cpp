36de35a11fe0192e04a599a2cdf3e59789777364
ilovefriedchick <fikrihyderba04.24@ichat.sp.edu.sg>
2024-05-21 11:46:58 +0800

    Create MuhammadFikriHyder_P293343_DCPE1B24.cpp

---
+++ MuhammadFikriHyder_P293343_DCPE1B24.cpp
@@ -0,0 +1,44 @@
+
+
+#include <iostream>
+#include <cmath> 
+using namespace std;
+
+int main()
+{
+    int age;
+    double height, result;
+
+    cout << "This program is done by.." << "\n";
+    cout << "Name: Fikri Hyder " << "\n";
+    cout << "Adm no: P293343" << "\n\n";
+
+    cout << "Enter your age: " << "\n";
+    cin >> age;
+
+    if (age > 16)
+    {
+        cout << "Enter your height in meters: " << "\n";
+        cin >> height;
+
+        result = -100.0 / 26 + sqrt(pow((pow(age, 2) - 16), 3)) / pow(height, 3) + 55;
+        cout << "The computed result is " << result << "\n";
+    }
+    else {
+        cout << "Your age must be more than 16." << "\n";
+    }
+
+    return 0; 
+}
+
+	
+	
+	
+	
+
+
+
+
+
+
+
